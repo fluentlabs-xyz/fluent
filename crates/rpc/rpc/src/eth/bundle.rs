7ea9cf82f340c50922bbd86e6a1f3b0cd355944c
Stas Stepanov <stanislav@ankr.com>
2024-10-11 19:19:45 +0700

    update fluentbase

--- crates/rpc/rpc/src/eth/bundle.rs
+++ crates/rpc/rpc/src/eth/bundle.rs
@@ -117,7 +117,7 @@ where
                 let mut hash_bytes = Vec::with_capacity(32 * transactions.len());
 
                 let mut evm =
-                    revm::Evm::builder().with_db(db).with_env_with_handler_cfg(env).build_revm();
+                    revm::Evm::builder().with_db(db).with_env_with_handler_cfg(env).build();
 
                 let mut results = Vec::with_capacity(transactions.len());
                 let mut transactions = transactions.into_iter().peekable();
